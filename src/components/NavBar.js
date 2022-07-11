a76fb7a3eaaeb53a07fcfe03cb49e6d353124c45
Rachel <rstark.smith@gmail.com>
2022-07-11 12:16:41 -0500

    reinstall material ui to fix errors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const NavBar = () => {
     return (
-    <div>NavBar</div>
+        <div>
+            <ul>
+                <li><a class="active" href="#home">Home</a></li>
+                <li><a href="#news">Full List</a></li>
+                <li><a href="#contact">Add New Set</a></li>
+            </ul>
+        </div>
     )
 }
 
