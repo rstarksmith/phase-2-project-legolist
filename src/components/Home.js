91da5245a075ed0d30d7a3b781323ea00dfd2023
Rachel <rstark.smith@gmail.com>
2022-07-15 15:03:24 -0500

    card css

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,12 +7,12 @@ const Home = () => {
     const navigateToList = () => {
         history.push('/sets')
     }
-    
+
     return (
         <div>
-            <img src='https://c4.wallpaperflare.com/wallpaper/536/402/410/awesome-black-lego-logo-alternate-entertainment-other-hd-art-wallpaper-preview.jpg' alt='Lego List title graphic'></img>
-            <h3>Keep track of your LEGO collection with LEGO List</h3>
-            <button onClick={navigateToList}>View List</button>
+            <img className='home-logo' src='https://c4.wallpaperflare.com/wallpaper/536/402/410/awesome-black-lego-logo-alternate-entertainment-other-hd-art-wallpaper-preview.jpg' alt='Lego List title graphic' /><br/>
+            <h3 className='home-description' >Keep track of your LEGO collection with LEGO List</h3>
+            <button className='home-button' onClick={navigateToList}>View List</button>
         </div>
     )
 }
