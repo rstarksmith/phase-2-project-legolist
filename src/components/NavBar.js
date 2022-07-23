30832dd7d8639239ae4607356371c5b486785a26
Rachel <rstark.smith@gmail.com>
2022-07-23 12:20:52 -0500

    refactor NavLinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 
 const NavBar = ({ reRenderSet }) => {
@@ -9,9 +9,9 @@ const NavBar = ({ reRenderSet }) => {
             <img src='https://i.imgur.com/tLdkxvn.png?1' alt='lego list logo' className="logo"/>
                 <ul className="nav-links">
                     <div className="menu">
-                        <Link to="/">Home</Link>
-                        <Link to="/sets" onClick={reRenderSet}>List</Link>
-                        <Link to="/sets/new">Add Set</Link>
+                        <NavLink exact to="/">Home</NavLink>
+                        <NavLink exact to="/sets" onClick={reRenderSet}>List</NavLink>
+                        <NavLink to="/sets/new">Add Set</NavLink>
                     </div>
                 </ul>
         </nav>
